4be334316b3fb1cf2e36a31e3f286ae615d31aac
Leah Ellis <leahellis.1@hotmail.com>
2025-03-28 17:21:13 +0000

    Add EDTA TE annotation script

---
+++ edta_annotate_te.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --partition=defq
+#SBATCH --nodes=16
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=50g
+#SBATCH --time=24:00:00
+#SBATCH --job-name=EDTA_c_amara
+#SBATCH --output=/share/BioinfMSc/rot3_group1/logs/%x-%j.out
+#SBATCH --error=/share/BioinfMSc/rot3_group1/logs/%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=youremail@email.com
+
+# Activate Conda environment
+source $HOME/.bash_profile
+conda activate EDTA
+
+# Move into cloned EDTA github directory
+cd /share/BioinfMSc/rot3_group1/EDTA/EDTA
+
+# Path to data directory
+c_amara_genome=/share/BioinfMSc/rotation3/cardamine_amara_v0.9
+
+# Annotate transposable elements for haplome 1
+perl EDTA.pl --genome $c_amara_genome/C087_203_mapq_hap1_8.fa \
+    --overwrite 1 \
+    --anno 1 \
+    -t 16
+
+# Annotate transposable elements for haplome 2
+perl EDTA.pl --genome $c_amara_genome/C087_203_mapq_hap2_8.fa \
+    --overwrite 1 \
+    --anno 1 \
+    -t 16
+
+# Deactivate conda environment
+conda deactivate
+
