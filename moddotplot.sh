4ee909e042c93d4d6fbad0ca7872050d1a597e31
Leah Ellis <leahellis.1@hotmail.com>
2025-04-14 22:08:05 +0100

    Specify ModDotPlot use for entire haplomes

--- moddotplot.sh
+++ moddotplot.sh
@@ -17,19 +17,12 @@
 source $HOME/.bash_profile
 
 # Paths
-haplome_dir=/path/to/cardamine_amara_haplomes
-subset_haplome_dir=/path/to/cardamine_amara_genome_analysis/SamTools
+haplome_data_dir=/path/to/cardamine_amara_haplomes
 out_dir=/path/to/cardamine_amara_genome_analysis/ModDotPlot
 
 # Generate identity maps for the entire genome
 moddotplot static -f $haplome_dir/C087_203_mapq_hap1_8.fa --compare -o $out_dir/haplome1
 moddotplot static -f $haplome_dir/C087_203_mapq_hap2_8.fa --compare -o $out_dir/haplome2
-
-# Generate identity maps for the chromosomes of interest
-moddotplot static -f $subset_haplome_dir/h1rl5_h2rl2.fasta --compare -o $out_dir/rl5vrl2
-
-# Generate identity maps for the blocks of interest
-moddotplot static -f $subset_haplome_dir/hap1rl5hap2rl2blocks.fasta --compare -o $out_dir/rl5vrl2block
 
 # Deactivate conda
 conda deactivate
