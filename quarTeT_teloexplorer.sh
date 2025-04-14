7062cf6740f6baf0a58f807e224b9b529fb47391
Leah Ellis <leahellis.1@hotmail.com>
2025-04-14 22:20:53 +0100

    Alter file paths

--- quarTeT_teloexplorer.sh
+++ quarTeT_teloexplorer.sh
@@ -7,8 +7,8 @@
 #SBATCH --mem=30g
 #SBATCH --time=24:0:00
 #SBATCH --job-name=quarTeT_teloexplorer_camara
-#SBATCH --output=/share/BioinfMSc/rot3_group1/logs/%x-%j.out
-#SBATCH --error=/share/BioinfMSc/rot3_group1/logs/%x-%j.err
+#SBATCH --output=/path/to/cardamine_amara_genome_analysis/logs/%x-%j.out
+#SBATCH --error=/path/to/cardamine_amara_genome_analysis/logs/%x-%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=youremail@email.com
 
@@ -17,19 +17,19 @@ source $HOME/.bash_profile
 conda activate quarTeT
 
 # Move to cloned quarTeT github directory
-cd /share/BioinfMSc/rot3_group1/quarTeT/quarTeT
+cd /path/to/cardamine_amara_genome_analysis/quarTeT
 
 # File paths
-genome_dir=/share/BioinfMSc/rotation3/cardamine_amara_v0.9
+haplome_data_dir=/path/to/cardamine_amara_haplomes
 
 # Predict telomeres for haplome 1
-python3 quartet.py TeloExplorer -i $genome_dir/C087_203_mapq_hap1_8.fa \
+python3 quartet.py TeloExplorer -i $haplome_data_dir/C087_203_mapq_hap1_8.fa \
     -c plant \
     -m 80 \
     -p hap1_8_telo
 
 # Predict telomeres for haplome 2
-python3 quartet.py TeloExplorer -i $genome_dir/C087_203_mapq_hap2_8.fa \
+python3 quartet.py TeloExplorer -i $haplome_data_dir/C087_203_mapq_hap2_8.fa \
     -c plant \
     -m 80 \
     -p hap1_8_telo
