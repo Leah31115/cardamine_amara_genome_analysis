3b4fb8488204288a9f9c6d9d72aa8d84b591e653
Leah Ellis <leahellis.1@hotmail.com>
2025-04-14 22:18:54 +0100

    Alter file paths

--- quarTeT_centrominer.sh
+++ quarTeT_centrominer.sh
@@ -7,8 +7,8 @@
 #SBATCH --mem=30g
 #SBATCH --time=34:00:00
 #SBATCH --job-name=quarTeT_centro_camara
-#SBATCH --output=/share/BioinfMSc/rot3_group1/logs/%x-%j.out
-#SBATCH --error=/share/BioinfMSc/rot3_group1/logs/%x-%j.err
+#SBATCH --output=/path/to/cardamine_amara_genome_analysis/logs/%x-%j.out
+#SBATCH --error=/path/to/cardamine_amara_genome_analysis/logs/%x-%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=youremail@email.com
 
@@ -17,23 +17,23 @@ source $HOME/.bash_profile
 conda activate quarTeT
 
 # Move to cloned quarTeT github directory
-cd /share/BioinfMSc/rot3_group1/quarTeT/quarTeT
+cd /path/to/cardamine_amara_genome_analysis/quarTeT
 
 # File paths
-genome_dir=/share/BioinfMSc/rotation3/cardamine_amara_v0.9
-edta_dir=/share/BioinfMSc/rot3_group1/EDTA/EDTA
-augustus_data_dir=/share/BioinfMSc/rot3_group1/Augustus/c_amara
+haplome_data_dir=/path/to/cardamine_amara_haplomes
+edta_data_dir=/path/to/cardamine_amara_genome_analysis/EDTA
+augustus_data_dir=/path/to/cardamine_amara_genome_analysis/augustus
 
 # Predict centromeres for haplome 1
-python3 quartet.py CentroMiner -i $genome_dir/C087_203_mapq_hap1_8.fa \
-    --TE $edta_dir/C087_203_mapq_hap1_8.fa.mod.EDTA.TEanno.gff3 \
+python3 quartet.py CentroMiner -i $haplome_data_dir/C087_203_mapq_hap1_8.fa \
+    --TE $edta_data_dir/C087_203_mapq_hap1_8.fa.mod.EDTA.TEanno.gff3 \
     --gene $augustus_data_dir/c_amara_hap1.gff3 \
     -p hap1_8_centro \
     -t 8
 
 # Predict centromeres for haplome 2
-python3 quartet.py CentroMiner -i $genome_dir/C087_203_mapq_hap2_8.fa \
-    --TE $edta_dir/C087_203_mapq_hap2_8.fa.mod.EDTA.TEanno.gff3 \
+python3 quartet.py CentroMiner -i $haplome_data_dir/C087_203_mapq_hap2_8.fa \
+    --TE $edta_data_dir/C087_203_mapq_hap2_8.fa.mod.EDTA.TEanno.gff3 \
     --gene $augustus_data_dir/c_amara_hap2.gff3 \
     -p hap2_8_centro \
     -t 8
