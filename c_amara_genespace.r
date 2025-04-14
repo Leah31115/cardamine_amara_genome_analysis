4ac290222c98e11f708b0d4536de4466e2b1b963
Leah Ellis <leahellis.1@hotmail.com>
2025-04-14 22:00:54 +0100

    Alter file path

--- c_amara_genespace.r
+++ c_amara_genespace.r
@@ -2,7 +2,7 @@
 library(GENESPACE)
 
 # Paths
-wd <- "/share/BioinfMSc/rot3_group1/GENESPACE"
+wd <- "/path/to/cardamine_amara_genome_analysis/GENESPACE"
 path2mcscanx <- "~/LIFE4137-HPC_practice/exit/envs/genespace4/bin" # Path to your conda environment bin dir
 
 # Initialise the genespace run and set the run parameters
